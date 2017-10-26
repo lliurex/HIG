1d0977e18b21c8e4c61c10c28abb3dc626ef17fc
Raul Rodrigo Segura <raurodse@gmail.com>
2017-10-26 09:29:47 +0200

    fix style

--- build/build.js
+++ build/build.js
@@ -1,11 +1,14 @@
-var markdown = require( "markdown" ).markdown;
+// var markdown = require( "markdown" ).markdown;
+var showdown = require( "showdown" );
+var markdown = new showdown.Converter();
 var fs = require('fs');
 var stream = fs.createWriteStream("index.html");
 stream.once('open', function(fd) {
     fs.readFile('build/head', 'utf8', function (err,data) {
         stream.write(data);
         fs.readFile('src/index.md', 'utf8', function (err,data) {
-            var content1 = markdown.toHTML(data);
+            // var content1 = markdown.toHTML(data);
+            var content1 = markdown.makeHtml(data);
             stream.write(content1);
             fs.readFile('build/foot', 'utf8', function (err,data) {
                 stream.write(data);
